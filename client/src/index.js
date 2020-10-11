78c86f57f085155a5b3e8a0e76b166ece41a6198
Ritvik <ritvyk@gmail.com>
2020-10-11 16:11:47 +0530

    Added client

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import PinlyApp from "./pinlyApp";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PinlyApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ client/src/pinlyApp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PinlyApp() {
+  return <div className="bg-gray-200 w-screen h-screen">Hello</div>;
+}
+
+export default PinlyApp;
